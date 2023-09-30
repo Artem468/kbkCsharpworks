7c92045830c4c3fb4148de882aecb9739720e889
Artem <artem.belykh.ru@gmail.com>
2023-09-30 10:19:25 +0200

    кк

---
+++ lab2Belikh_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Высокий уровень 21
+
+Console.Write("Введите число от 1 до 31: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day >= 1 && day <= 31)
+{
+    int nextNewmoon = (27 + 28) % 31;
+
+    Console.WriteLine(day <= 27 ? "27 августа" : $"{nextNewmoon} сентября");
+}
+else
+{
+    Console.WriteLine("Число не входит в допустимый диапозон");
+}
