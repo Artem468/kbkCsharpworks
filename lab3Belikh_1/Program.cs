710a902f731590477930fd92f86c9de5ccbc75d3
Artem468 <56316884+Artem468@users.noreply.github.com>
2023-10-18 20:59:47 +0200

    Add files via upload

---
+++ lab3Belikh_1/Program.cs
@@ -0,0 +1,26 @@
+﻿try
+{
+    Console.Write("Введите x: ");
+    double x = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Введите n: ");
+    double n = Convert.ToDouble(Console.ReadLine());
+
+    double Q = 0;
+
+    for (int k = 1; k <= n; k++)
+    {
+        double Kfactorial = 1;
+        for (int i = 1; i <= k; i++)
+        {
+            Kfactorial *= i;
+        }
+        Q += (Math.Pow(-1, k) * Math.Pow((x + 3), 2)) / Kfactorial;
+    }
+
+    Console.WriteLine($"Q = {Q:F2}");
+}
+catch (Exception err)
+{
+    Console.WriteLine($"Возникла ошибка: {err}");
+}
