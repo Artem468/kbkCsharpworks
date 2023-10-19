b46fa2ddfb372d68666770abbb535cbcba5940c5
Artem468 <56316884+Artem468@users.noreply.github.com>
2023-10-19 17:51:28 +0200

    Add files via upload

---
+++ lab3Belikh_2/Program.cs
@@ -0,0 +1,21 @@
+﻿try
+{
+    Console.WriteLine("Введите числа");
+    double number = Convert.ToDouble(Console.ReadLine());
+    double sum = 0;
+    while (number != 0)
+    {
+        if (number < 0)
+        {
+            sum += number;
+        }
+
+        number = Convert.ToDouble(Console.ReadLine());
+    }
+
+    Console.WriteLine($"Сумма отрицательных числел: {sum:F2}");
+}
+catch (Exception err)
+{
+    Console.WriteLine($"Ошибка: {err}");
+}
