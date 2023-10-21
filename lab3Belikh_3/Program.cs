933b2b320bef8628687c1441ebf23bc9d1a31b65
Artem468 <56316884+Artem468@users.noreply.github.com>
2023-10-21 15:49:33 +0200

    Add files via upload

---
+++ lab3Belikh_3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double S = 1;
+
+for (int i = 1; i <= n; i++)
+{
+    long factorial = 1;
+    for (int j = 1; j <= i; j++)
+    {
+        factorial *= j;
+    }
+    
+    S += 2 * i + 1 * Math.Pow(x, i * 2) / factorial;
+}
+
+Console.WriteLine($"S = {S:F2}");
