e48b2911d613671f87783717f8b3d9885c4950f9
Artem468 <56316884+Artem468@users.noreply.github.com>
2023-10-23 18:53:07 +0200

    Add files via upload

---
+++ lab3Belikh_4/Program.cs
@@ -0,0 +1,14 @@
+﻿for (double x = -1; x <= 1; x += 0.25)
+{
+    double u = x * x - Math.Asin(x * x);
+    if (x > Math.PI)
+    {
+        double output = Math.Pow(u, Math.Abs(Math.Cos(x)));
+        Console.WriteLine($"{x, 5} | {output:F2}");
+    }
+    else if (x <= Math.PI)
+    {
+        double output = Math.Pow(u, Math.Sin(x));
+        Console.WriteLine($"{x, 5} | {output:F2}");
+    }
+}
