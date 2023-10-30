736b5b9f18e07c953621d03021cb493093831cd4
Artem468 <56316884+Artem468@users.noreply.github.com>
2023-10-30 21:11:54 +0200

    Add files via upload

---
+++ lab4Belikh_1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int count = Convert.ToInt32(Console.ReadLine());
+double[] massive = new double[count];
+
+for (int j = 0; j < count; j++)
+{
+    Console.Write("Введите число: ");
+    massive[j] = Convert.ToInt32(Console.ReadLine());
+}
+
+int i = 1;
+double denominator = 0;
+
+bool flag = true;
+
+while (i < massive.Length)
+{
+    if (massive[i] / massive[i - 1] != denominator && denominator != 0)
+    {
+        Console.WriteLine("Не является геометрической последовательностью!");
+        flag = false;
+        break;
+    }
+    else { denominator = massive[i] / massive[i - 1]; }
+    i++;
+} 
+
+if (flag) { Console.WriteLine($"Является геометрической последовательнотью с знаменателем {denominator}"); }
