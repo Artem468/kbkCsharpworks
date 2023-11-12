851efdcd66dcc44e5aaf9e379ade995d86e185f5
Artem468 <56316884+Artem468@users.noreply.github.com>
2023-11-12 21:35:34 +0200

    Add files via upload

---
+++ lab5Belikh_1/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.Write("Введите текст: ");
+char[] massive = Console.ReadLine().ToCharArray();
+
+
+// Узнаем количество слов
+int wordsCount = 0;
+
+for (int i = 0; i < massive.Length; i++)
+{
+    if (char.IsWhiteSpace(massive[i]) || i == 0)
+    {
+        wordsCount++;
+    }
+}
+
+//  Узнаем индексы начала слов 
+int [] startIndexWords = new int[wordsCount];
+
+int indexNow = 0;
+for (int j = 0; j < massive.Length; j++)
+{
+    if (char.IsWhiteSpace(massive[j]))
+    {
+        startIndexWords[indexNow] = j + 1;
+        indexNow++;
+    }
+    else if (j == 0)
+    {
+        startIndexWords[indexNow] = j;
+        indexNow++;
+    }
+}
+
+// Меняем местами четные и не четные индескы
+for (int k = 0; k < startIndexWords.Length; k++)
+{
+    if (k % 2 == 1) { (startIndexWords[k], startIndexWords[k - 1]) = (startIndexWords[k - 1], startIndexWords[k]); }
+}
+
+// Вывод на экран
+foreach (int item in startIndexWords)
+{
+    // Console.WriteLine(massive[item]);
+    int index = item;
+    while (!char.IsWhiteSpace(massive[index]))
+    {
+        Console.Write($"{massive[index]}");
+        index++;
+        if (index == massive.Length) {break; }
+    };
+    Console.Write(" ");
+}
