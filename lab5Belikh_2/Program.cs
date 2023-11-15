45d7b626d0bb27afb39b25dc150c35ba556a101e
Artem468 <56316884+Artem468@users.noreply.github.com>
2023-11-15 18:45:57 +0200

    Add files via upload

---
+++ lab5Belikh_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Высокий уровень сложности. Задача № 9
+
+Console.Write("Введите строку: ");
+string input = Console.ReadLine();
+int count = 0;
+foreach (string word in input.Split(' '))
+{
+    bool flag = true;
+    foreach (char letter in word)
+    {
+        if (!(char.IsLower(letter) && letter >= 'a' && letter <= 'я'))
+        {
+            flag = false;
+        }
+    }
+
+    if (flag)
+    {
+        count++;
+    }
+    flag = true;
+}
+
+Console.WriteLine($"Количество слов состоящих только из строчных русскоих букв: {count}");
+
+
+
+// Более симпотичное решение с использованием регулярных выражений
+
+// using System.Text.RegularExpressions;
+//
+// Console.Write("Введите строку: ");
+// string input = Console.ReadLine();
+//
+// Regex regex = new Regex(@"\b[а-я]+\b");
+//
+// int count = regex.Matches(input).Count;
+// Console.WriteLine($"Количество слов состоящих только из строчных русскоих букв: {count}");
