5428c74a10956e305e1fe82a7203c13a7c03dcad
Artem468 <56316884+Artem468@users.noreply.github.com>
2023-11-25 16:17:01 +0200

    Add files via upload

---
+++ lab6Belikh_2/Library/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Library
+{
+    public class Matrix
+    {
+        public static double GetMaxSquares(double[,] matrix)
+        {
+            double[] vector = new double [matrix.GetLength(1)];
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                double maxItem = 0;
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    if (matrix[j, i] > maxItem)
+                    {
+                        maxItem = matrix[j, i];
+                    }       
+                }
+
+                vector[i] = maxItem;
+            }
+
+            double underAbs = 0;
+            foreach (double item in vector)
+            {
+                double mult = 1;
+                for (int k = vector.Length - 1; k <= vector.Length; k++)
+                {
+                    mult *= k;
+                } 
+                
+                underAbs += Math.Pow(item, 3) + mult;
+            }
+            return Math.Sqrt(Math.Abs(underAbs));
+        }
+        
+        
+    }
+}
---
+++ lab6Belikh_2/lab6Belikh_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using Library;
+
+try
+{
+    Console.Write("Введите N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите M: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+
+    double[,] massive = new double[n, m];
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            massive[i - 1, j - 1] = Math.Pow(Math.Pow(Math.Sin(i), 3) - Math.Pow(Math.Cos(j), 3), 3) +
+                            3.1 * Math.Log(Math.Abs(i - 7 / j + 4) - 9);
+        }
+    }
+
+    double result = Library.Matrix.GetMaxSquares(massive);
+    Console.WriteLine($"{result:F2}");
+
+}
+catch (Exception err)
+{
+    Console.WriteLine(err.Message);
+}
